4bb2595dbe815ebfd860f8653792b864d399185c
Sriabhinay Kusuma <sriabhinayk@gmail.com>
2024-06-24 17:07:28 -0500

    commit

--- src/app/api/demo-route-2/route.js
+++ src/app/api/demo-route-2/route.js
@@ -1,14 +1,15 @@
-// import axios from "axios"
-// export async function GET(request) {
-//     // const { searchParams } = new URL(request.url)
-//     // const id = searchParams.get('id')
-//     const res = await axios.get("https://jsonplaceholder.typicode.com/users", {
+import axios from "axios"
+export async function GET(request) {
+    // // const { searchParams } = new URL(request.url)
+    // // const id = searchParams.get('id')
+    // const res = await axios.get("https://jsonplaceholder.typicode.com/users", {
       
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     })
-//     const users = await res.text();
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    // })
+    // const users = await res.text();
    
-//     return Response.json({ users })
-//   }
+    // return Response.json({ users })
+    return Response('Hi')
+  }
--- src/app/demo-get-2/page.jsx
+++ src/app/demo-get-2/page.jsx
@@ -1,17 +1,17 @@
-// 'use client';
-// import React, { useState } from 'react';
+'use client';
+import React, { useState } from 'react';
 
-// function Page() {
-//     const apiCall = async () => {
-//         let data = await fetch('/api/demo-route-2',{
-//             method: 'GET',
-//         })
-//         console.log(await data.json())
-//     }
-//     return <div>
-//         <button onClick={apiCall}>api call</button>
-        
-//     </div>
+function Page() {
+    // const apiCall = async () => {
+    //     let data = await fetch('/api/demo-route-2',{
+    //         method: 'GET',
+    //     })
+    //     console.log(await data.json())
+    // }
+    return <div>
+        {/* <button onClick={apiCall}>api call</button>
+         */}
+    </div>
 
-// }
-// export default Page;
+}
+export default Page;
