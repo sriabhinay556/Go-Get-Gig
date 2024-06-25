a8c74f46c51b986197b2ae46fa917aaabe4b8127
Sriabhinay Kusuma <sriabhinayk@gmail.com>
2024-06-25 12:55:22 -0500

    roll back

--- src/app/api/cron/route.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-    console.log("from cron/route.js")
-  return NextResponse.json({ ok: "message from cron/route.js" });
-}
